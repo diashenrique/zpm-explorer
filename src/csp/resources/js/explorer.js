1e5afd4969996d50b63ede0d3ef1b251d6331fec
Henrique Dias <henrique.dias@gmail.com>
2021-04-18 10:39:50 -0300

    Install packages using UI

--- src/csp/resources/js/explorer.js
+++ src/csp/resources/js/explorer.js
@@ -25,8 +25,7 @@ $(document).ready(function () {
 
   $("#package-list").dxDataGrid({
     dataSource: customStore,
-    rowAlternationEnabled: true,
-    allowColumnResizing: true,
+    rowAlternationEnabled: false,
     columnResizingMode: "widget",
     //columnAutoWidth: true,
     allowColumnResizing: true,
@@ -43,7 +42,7 @@ $(document).ready(function () {
       showInfo: true
     },
     sorting: {
-      mode: "multiple"
+      mode: "single"
     },
     filterRow: {
       visible: true,
@@ -57,15 +56,71 @@ $(document).ready(function () {
     headerFilter: {
       visible: true
     },
-    columns: ["name","description",{
+    selection: {
+      mode: "single"
+    },
+    focusedRowEnabled: true,
+    columns: ["name", "description", {
       dataField: "repository",
       cellTemplate: function (container, options) {
-          var linkRepository = options.data.repository;
-          container.append($("<a>").addClass('repoLink').text(linkRepository).on("click", function (args) {
-              openDetails(linkRepository);
-          }).appendTo(container));
+        var linkRepository = options.data.repository;
+        container.append($("<a>").addClass('repoLink').text(linkRepository).on("click", function (args) {
+          openDetails(linkRepository);
+        }).appendTo(container));
       }
-    }]
+    }],
+    onSelectionChanged: function (selectedItems) {
+      var data = selectedItems.selectedRowsData[0];
+      if (data) {
+        //console.log(data.name);
+      }
+    },
+    onToolbarPreparing: function (e) {
+      var dataGrid = e.component;
+
+      e.toolbarOptions.items.push({
+        location: "after",
+        widget: "dxButton",
+        options: {
+          icon: "fas fa-download",
+          text: "Install",
+          hint: "Install the selected package",
+          onClick: function (e) {
+
+            var selectedRowsData = dataGrid.getSelectedRowsData();
+
+            console.log(selectedRowsData.length);
+
+
+            var idSelecionado = $("#selected-items-container").text();
+
+            if (selectedRowsData.length === 0) {
+              DevExpress.ui.notify("No package have been selected", "error");
+            } else {
+              var result = DevExpress.ui.dialog.confirm("Do you want to install the package <b>"+`${selectedRowsData[0].name}` + "</b> ?", "Install Package");
+              result.done(function (resp) {
+                if (resp) {
+                  var values = {
+                    id: idSelecionado,
+                    namespace: selectedNamespace
+                  };
+                  $.ajax({
+                    url: urlREST + "/message/resend/",
+                    method: "POST",
+                    processData: false,
+                    contentType: "application/json",
+                    data: JSON.stringify(values)
+                  }).done(function () {
+                    DevExpress.ui.notify("Messages were successfully resent", "success", 4000);
+                  });
+                }
+              });
+
+            }
+          }
+        }
+      });
+    },
   });
 
 
